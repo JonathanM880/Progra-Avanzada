daf6815b19143f0a88ea1b3252e8e3a6b4c6b00d
JonathanM880 <suarezjona3399@gmail.com>
2025-04-30 17:46:15 -0500

    Add files via upload

--- primitivasinicio2025/Line.java
+++ primitivasinicio2025/Line.java
@@ -6,72 +6,68 @@ import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.nio.FloatBuffer;
 
-//clase de la primitiva
-// Opengl trabaja con identificadores (ids)
-public class Point {
+public class Line {
     private final FloatBuffer vertexBuffer; //buffer de memoria que guarda coordenadas de vertices
-    private final int mProgram; //combinar vertex shader con fragment shader
+    private final int mProgram; //combinar vertex shader con fragment shader (para ejecutar)
     private int positionHandle; //pasarle los datos de posición del vértice al shader.
     private int colorHandle; //almacena id del color
+    private final float[] coordLine = { //solo dos coordenadas
+            -0.9f, 0.9f,
+            0.9f,-0.9f};
+    float[] color = {1.0f,0.0f,0.0f,1.0f};
 
-    static final int COORDS_POR_VERTEX = 3; //El número de coordenadas por vértice (x, y, z).
-    static float pointCoord[] = {0.0f,0.0f,0.0f}; //coordenadas del punto (entre -1 y 1)
-    private final int vertexCount = pointCoord.length/COORDS_POR_VERTEX; //cuenta vertices que vamos a dibujar (el punto tiene un vertice)
-    private final int vertexStride = COORDS_POR_VERTEX*4; //determinar el tamaño de memoria de cada vertice || el programa sabe que cada 12 bytes tenemos un vertice
-    // siempre debe dar 12
-    float color[] = {1.0f,0.0f,0.0f,1.0f}; //color del punto || se puede hacer de otra forma
-
-    public Point(){
+    public Line(float[] coordLine) {
         // siempre necesarias
-        ByteBuffer byteBuffer = ByteBuffer.allocateDirect(pointCoord.length*4);
+        ByteBuffer byteBuffer = ByteBuffer.allocateDirect(coordLine.length * 4);
         byteBuffer.order(ByteOrder.nativeOrder()); //sirve para establecer el orden de bytes (endianness) del buffer
         vertexBuffer = byteBuffer.asFloatBuffer(); //convierte a floatBuffer para trabajar con floats en lugar de bytes
-        vertexBuffer.put(pointCoord); //Copia los valores del arreglo pointCoord al FloatBuffer
+        vertexBuffer.put(coordLine); //Copia los valores del arreglo pointCoord al FloatBuffer
         vertexBuffer.position(0); //para que empiece desde la posicion 0 del arreglo
 
-        int vertexShader = loadShaders(GLES20.GL_VERTEX_SHADER, vertexShaderCode);
-        int fragmentShader = loadShaders(GLES20.GL_FRAGMENT_SHADER, fragmentShaderCode);
+        int vertexShader = loadShaders(GLES20.GL_VERTEX_SHADER, vertexShaderCode); //generar id vertex
+        int fragmentShader = loadShaders(GLES20.GL_FRAGMENT_SHADER, fragmentShaderCode); //generar id fragment
 
         mProgram = GLES20.glCreateProgram(); //crea el programa
         GLES20.glAttachShader(mProgram,vertexShader); //agrega el vertex a la inicicializacion del programa
         GLES20.glAttachShader(mProgram,fragmentShader);
         GLES20.glLinkProgram(mProgram); //para compilar programa
+
     }
 
-    public void draw() {
+    public void draw(){
         GLES20.glUseProgram(mProgram);
 
-        positionHandle = GLES20.glGetAttribLocation(mProgram, "vPosition"); // identificador de la posicion
+        positionHandle = GLES20.glGetAttribLocation(mProgram, "vPosition");
         GLES20.glEnableVertexAttribArray(positionHandle);
 
         GLES20.glVertexAttribPointer(
-                positionHandle, COORDS_POR_VERTEX,
+                positionHandle, 2, //dimensiones, tamaño vertices
                 GLES20.GL_FLOAT, false,
-                vertexStride, vertexBuffer);
+                0, vertexBuffer); //puedo ponerle 0 al stride si no lo declaro arriba
 
         colorHandle = GLES20.glGetUniformLocation(mProgram, "vColor");
         GLES20.glUniform4fv(
                 colorHandle, 1, //numero de vectores que se va a enviar
                 color, 0); //donde se empiezan a leer los valores (indice arreglo)
-
-        GLES20.glDrawArrays(GLES20.GL_POINTS, 0, vertexCount);
-
-        GLES20.glDisableVertexAttribArray(positionHandle); //es como limpiar
+                      //Que vamos a dibujar||desde donde||vertices
+        GLES20.glLineWidth(10.0f);
+        GLES20.glDrawArrays(GLES20.GL_LINES, 0, 2);
+        GLES20.glDisableVertexAttribArray(positionHandle);
     }
+
 
     private final String vertexShaderCode =
             "attribute vec4 vPosition;"+ //es como una variable vec4 es como un tipo de dato y vPosition siempre debe llamarse asi
-            "void main(){"+
-            "gl_Position = vPosition;"+
-            "gl_PointSize = 100.0;"+ //tamaño punto, pixeles
-            "}"; //ATTRIBUTES
+                    "void main(){"+
+                    "gl_Position = vPosition;"+
+                    "}"; //ATTRIBUTES
 
     private final String fragmentShaderCode=
             "precision mediump float;"+
-            "uniform vec4 vColor;"+
-            "void main(){"+
-            "gl_FragColor =vColor; "+
-            "}"; //UNIFORMS
+                    "uniform vec4 vColor;"+
+                    "void main(){"+
+                    "gl_FragColor =vColor; "+
+                    "}"; //UNIFORMS
 
     private int loadShaders(int type, String shaderCode){ //para cargar los shaders
         int shader = GLES20.glCreateShader(type);
@@ -80,5 +76,4 @@ public class Point {
 
         return shader;
     }
-
 }
--- primitivasinicio2025/MyGLRenderer.java
+++ primitivasinicio2025/MyGLRenderer.java
@@ -9,10 +9,27 @@ import javax.microedition.khronos.opengles.GL10;
 //se encarga de hacer el dibujo
 public class MyGLRenderer implements GLSurfaceView.Renderer {
     private Point point;
+    private Line line;
+    private Line line1;
+    private Line line2;
+
+    private final float[] coordLine = { //solo dos coordenadas
+            -0.5f, 0.5f,
+            0.5f, 0.5f};
+    private final float[] coordLine2 = { //solo dos coordenadas
+            -0.3f, 0.3f,
+            0.2f,-0.2f};
+    private final float[] coordLine3 = { //solo dos coordenadas
+            -0.4f, 0.3f,
+            0.2f,-0.1f};
     @Override
     public void onSurfaceCreated(GL10 gl10, EGLConfig eglConfig) {
         GLES20.glClearColor(0.0f,0.0f,0.0f,1.0f);
         point = new Point();
+        line = new Line(coordLine);
+        line1 = new Line(coordLine2);
+        line2 = new Line(coordLine3);
+
     }
 
     @Override
@@ -24,5 +41,8 @@ public class MyGLRenderer implements GLSurfaceView.Renderer {
     public void onDrawFrame(GL10 gl10) {
         GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
         point.draw();
+        line.draw();
+        line1.draw();
+        line2.draw();
     }
 }
